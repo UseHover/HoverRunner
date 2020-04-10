ec97092e8cff38624b91f55071dcf5a2eb57c49c
Oluwatobi Akinpelu <akinpeluoluwatobiloba@gmail.com>
2020-04-10 19:21:45 +0100

    setting up action details activity

---
+++ app_main/src/main/java/com/usehover/testerv2/ui/action_details/ActionDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.usehover.testerv2.ui.action_details;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActionDetailsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app_main/src/main/java/com/usehover/testerv2/ui/login/LoginFragment.java
+++ app_main/src/main/java/com/usehover/testerv2/ui/login/LoginFragment.java
@@ -3,7 +3,6 @@ package com.usehover.testerv2.ui.login;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
