39528a191e55c1f660eac1b62cb967c205c89ee7
kutalek <davkutalek@gmail.com>
2020-06-10 09:08:47 -0700

    Fixes to build options and dependencies, very minor fixes to other things

--- app_main/src/main/java/com/hover/runner/ui/action_details/ActionDetailsFragment.java
+++ app_main/src/main/java/com/hover/runner/ui/action_details/ActionDetailsFragment.java
@@ -152,7 +152,7 @@ public class ActionDetailsFragment extends Fragment implements ParserClickListen
             builder.request(ActionDetailsActivity.actionId);
             builder.setEnvironment(Apis.getTestEnvMode());
             builder.style(R.style.myHoverTheme);
-            builder.initialProcessingMessage(getResources().getString(R.string.transaction_coming_up));
+//            builder.initialProcessingMessage(getResources().getString(R.string.transaction_coming_up));
 
             assert  actionExtra !=null;
             boolean hasValidVariables = true;
